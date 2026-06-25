27ec17d8f328b6762e1f18764bb28241e02001f9
Aryka Matsvei <matuxaar@gmail.com>
2026-06-25 18:20:49 +0300

    fix: init Koin on iOS before ComposeUIViewController

--- shared/src/iosMain/kotlin/com/biomech/app/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/biomech/app/MainViewController.kt
@@ -1,9 +1,15 @@
 package com.biomech.app
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.biomech.core.common.PlatformContext
+import com.biomech.core.di.initKoin
+import platform.UIKit.UIViewController
 
-fun MainViewController() = ComposeUIViewController(
-    configure = {
-        enforceStrictPlistSanityCheck = false
-    },
-) { App() }
+fun MainViewController(): UIViewController {
+    initKoin(PlatformContext())
+    return ComposeUIViewController(
+        configure = {
+            enforceStrictPlistSanityCheck = false
+        },
+    ) { App() }
+}
